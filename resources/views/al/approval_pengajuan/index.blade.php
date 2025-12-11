1fa43709078ad349b55ba59a889ad7adef360f27
agung-permana <agungpermana490@gmail.com>
2025-12-11 11:02:52 +0700

    approval al

--- resources/views/al/approval_pengajuan/index.blade.php
+++ resources/views/al/approval_pengajuan/index.blade.php
@@ -39,6 +39,64 @@
                     </div>
                 </div>
             </div>
+
+            <!-- DATA APPROVE -->
+            <div class="row mt-3" id="data-approve-section" style="display: block;">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header text-center">
+                            <strong>DATA APPROVE</strong>
+                        </div>
+                        <div class="card-body">
+                            <table class="table table-bordered text-center mb-0">
+                                <thead style="background: #f5f5f5;">
+                                    <tr>
+                                        <th style="width: 70px;">PILIH</th>
+                                        <th>AP</th>
+                                        <th>CIF</th>
+                                        <th>NAMA</th>
+                                        <th>TANGGAL TRANSAKSI</th>
+                                        <th>KELOMPOK</th>
+                                        <th>NOMINAL</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td><input type="checkbox"></td>
+                                        <td>CIAWI</td>
+                                        <td>111DSC</td>
+                                        <td>LILIS NURJANAH</td>
+                                        <td>19-10-2024</td>
+                                        <td>001-0234</td>
+                                        <td>250.000</td>
+                                    </tr>
+
+                                    <tr>
+                                        <td><input type="checkbox"></td>
+                                        <td>CIAWI</td>
+                                        <td>433ADA</td>
+                                        <td>ITA MARSITA</td>
+                                        <td>19-10-2024</td>
+                                        <td>001-974</td>
+                                        <td>75.000</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                        <div class="card-footer">
+                            <button class="btn btn-success mr-2">
+                                <i class="fas fa-check"></i> Approve
+                            </button>
+                            <button class="btn btn-danger">
+                                <i class="fas fa-times"></i> Reject
+                            </button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 @endsection
